7b525b6620526aa5308cd15076bc6b3345a4a8b1
Sauravjha811 <93691654+Sauravjha811@users.noreply.github.com>
2023-03-03 12:54:30 +0530

    Add files via upload

---
+++ Pattern/basicPattern.java
@@ -0,0 +1,20 @@
+package pattern;
+import java.util.Scanner;
+public class basicPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=0;
+        while(i<n)
+        {
+            int j=0;
+            while(j<n)
+            {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
